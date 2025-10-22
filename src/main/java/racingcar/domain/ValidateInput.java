92071ff52df745a3aa098c38f0adb3110c3cf544
SoungJuyeon <tjdwndus1325@gmail.com>
2025-10-22 21:48:49 +0900

    feat: 시도 횟수 예외 처리

--- src/main/java/racingcar/domain/ValidateInput.java
+++ src/main/java/racingcar/domain/ValidateInput.java
@@ -1,9 +1,14 @@
 package racingcar.domain;
 
-public class ValidateCarName {
+public class ValidateInput {
     public static void validateCarName(String carNames) {
         if(carNames ==null || carNames.isEmpty()) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public static void validateTryCount(int tryCount) {
+        if(tryCount <= 0)
+            throw new IllegalArgumentException();
     }
 }
