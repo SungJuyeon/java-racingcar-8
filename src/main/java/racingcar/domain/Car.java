2e1b6748339883d9ff160e0a8479482e0d5ed1f9
SoungJuyeon <tjdwndus1325@gmail.com>
2025-10-22 21:38:52 +0900

    feat: Car 위치 초기화

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,11 @@
 package racingcar.domain;
 
 public class Car {
+    private String carName;
+    private int position;
+
+    public Car(String carName) {
+        this.carName = carName;
+        position = 0;
+    }
 }
