5d27fc1520a96daae0773f1bd7748c610e5834b1
SoungJuyeon <tjdwndus1325@gmail.com>
2025-10-23 10:48:14 +0900

    feat: 차수별 실행 결과 출력

--- src/main/java/racingcar/view/OutputHandler.java
+++ src/main/java/racingcar/view/OutputHandler.java
@@ -1,4 +1,20 @@
 package racingcar.view;
 
+import racingcar.domain.Cars;
+
 public class OutputHandler {
+    private static final String ROUND_RESULT_MESSAGE = "실행 결과";
+    private static final String POSITION_PRINT = "-";
+
+    public void roundResultMessage() {
+        System.out.println(ROUND_RESULT_MESSAGE);
+    }
+
+    public void roundResultPrint(Cars cars) {
+        for(int i = 0; i < cars.carCount(); i++) {
+            System.out.print(cars.get(i).getCarName() + " : ");
+            System.out.println(POSITION_PRINT.repeat(cars.get(i).getPosition()));
+        }
+        System.out.println();
+    }
 }
