4e8dfe3ecea1fb5d9a49cff82428481d3c0d7a07
SoungJuyeon <tjdwndus1325@gmail.com>
2025-10-23 18:50:34 +0900

    feat: 우승자 출력

--- src/main/java/racingcar/view/OutputHandler.java
+++ src/main/java/racingcar/view/OutputHandler.java
@@ -6,16 +6,21 @@ import racingcar.domain.Cars;
 public class OutputHandler {
     private static final String ROUND_RESULT_MESSAGE = "실행 결과";
     private static final String POSITION_PRINT = "-";
+    private static final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
 
-    public static void roundResultMessage() {
+    public void roundResultMessage() {
         System.out.println(ROUND_RESULT_MESSAGE);
     }
 
-    public static void printRoundResult(Cars cars) {
+    public void printRoundResult(Cars cars) {
         for(Car car : cars.getCars()) {
             System.out.print(car.getCarName() + " : ");
             System.out.println(POSITION_PRINT.repeat(car.getPosition()));
         }
         System.out.println();
+    }
+
+    public void printWinners(String winners) {
+        System.out.print(FINAL_WINNER_MESSAGE + winners);
     }
 }
