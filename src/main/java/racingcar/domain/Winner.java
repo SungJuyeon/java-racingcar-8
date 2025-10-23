ebc0164506b6abfd7370d49563c12a6fd6c79135
SoungJuyeon <tjdwndus1325@gmail.com>
2025-10-23 18:49:49 +0900

    feat: 우승자 결정

--- src/main/java/racingcar/domain/Winner.java
+++ src/main/java/racingcar/domain/Winner.java
@@ -2,20 +2,22 @@ package racingcar.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class DecisionWinner {
+public class Winner {
+    private static final String JOIN_WINNERS_NAME = ",";
     List<String> winners;
 
-    public DecisionWinner() {
+    public Winner() {
         winners = new ArrayList<>();
     }
 
-    public static List<String> decideWinners(Cars cars) {
+    public static String decideWinners(Cars cars) {
         int maxPosition = cars.getMaxPosition(cars);
-        return cars.getCars().stream()
+        List<String> winners = cars.getCars().stream()
                 .filter(Car -> Car.getPosition() == maxPosition)
                 .map(Car::getCarName)
                 .toList();
+
+        return String.join(JOIN_WINNERS_NAME, winners);
     }
 }
