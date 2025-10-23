e0bd046b26720467f14aedf85d037cd34fb47a4c
SoungJuyeon <tjdwndus1325@gmail.com>
2025-10-23 18:51:32 +0900

    feat: controller에서 우승자 계산, 출력

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,18 +1,23 @@
 package racingcar.controller;
 
 import racingcar.domain.Cars;
+import racingcar.domain.Winner;
 import racingcar.domain.RacingGame;
 import racingcar.domain.ValidateInput;
 import racingcar.view.InputHandler;
 import racingcar.view.OutputHandler;
 
+import static racingcar.domain.Winner.decideWinners;
+
 public class RacingController {
     private final InputHandler inputHandler;
     private final RacingGame racingGame;
+    private final OutputHandler outputHandler;
 
     public RacingController() {
         inputHandler = new InputHandler();
         racingGame = new RacingGame();
+        outputHandler = new OutputHandler();
     }
 
     public void start() {
@@ -26,14 +31,19 @@ public class RacingController {
 
         startGame(tryCount, cars);
 
-
+        announceWinner(cars);
     }
 
     public void startGame(int tryCount, Cars cars) {
-        OutputHandler.roundResultMessage();
+        outputHandler.roundResultMessage();
         while(tryCount-- > 0) {
             racingGame.playRound(cars);
-            OutputHandler.roundResultPrint(cars);
+            outputHandler.printRoundResult(cars);
         }
+    }
+
+    public void announceWinner(Cars cars) {
+        String decideWinner = decideWinners(cars);
+        outputHandler.printWinners(decideWinner);
     }
 }
